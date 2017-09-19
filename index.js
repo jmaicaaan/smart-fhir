752f114517bf01da6bccf584b3905da2e53b14d8
Cedrick S. Mandocdoc <cmandocdoc@isbx.com>
2017-09-19 08:41:59 +0800

    Use client secret over http

--- index.js
+++ index.js
@@ -10,42 +10,35 @@ let server = {
   domain: "localhost"
 };
 
-let user = {
-  iss: "http://localhost:3000",
-  sub: "5b483eab-fdd6-46bb-aae4-f053fed5d3c4",
-  exp: 1422568860,
-  aud: "https://sb-auth.smarthealthit.org/token",
-  jti: "c0632b33-b83c-4302-89ad-4faf1a1ae0e0",
-  token_endpoint_auth_method: "private_key_jwt"
-};
-
-let key = fs.readFileSync('./src/keygen/id_rsa');
-
-let token = jwt.sign(user, key, { algorithm: "RS256" });
-
-// let secret = new Buffer("secret").toString('base64')
-//
-// let token = jwt.sign(user, secret);
+let clientSecret = "AMJoV_D0jiJeptbBKhxMJCv5tab6CzisnFFU7aeiBZALNiBjPGKu24M07x9fxmCE4SGdlu8vNYN9cPhxDf_K0ko";
+let clientId = "8adbf510-2181-4ae3-afd6-277935b6b3cd";
 
 let form = {
   grant_type: "client_credentials",
-  scope: "launch system/*.read profile",
+  scope: "online_access profile",
   client_assertion_type: "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
-  client_assertion: token,
-  token_endpoint_auth_method: "private_key_jwt",
-  "jwks": {
-    "keys": [
-      {
-        "kty": "RSA",
-        "e": "AQAB",
-        "use": "sig",
-        "kid": "secret",
-        "alg": "RS256",
-        "n": "hP8au9TzqnaLm1cav2FH49xsq-AF6oe4JTKP4Yevh3O39V4btv51lbkysJ-GYmoKqWEqM_fhnTmOhyqbL2z_zY4tfhkHJ6_pAfJ7jlSH3OiAdavZTnuQBd5tMyoQvCugZ7iyJm6T-MqBjZIJnSxBl_8UjIawAZGX9jx6owHwd1IjtSQCBVodA_OA9cO9aD5I7B80pMQvr4MfJ3Ud2N59DTS7o_YHzuPz1Ugr0XKiqqPL-NcepjjbL13AIp6HpKO8vpUmKTWWW2OuKT7S0TwM87lTQ3WX9C9Py2NPYwl0JDgpyRRI7yJ7ucbJXSWIs35gvw4k7M75VkO8_5qlMINvIQ"
-      }
-    ]
-  }
-};
+  client_id: clientId,
+  client_secret: clientSecret
+}
+
+// let claims = {
+//   iss: "http://localhost:3000",
+//   sub: clientId,
+//   exp: 1422568860,
+//   aud: "https://sb-auth.smarthealthit.org/token"
+// };
+//
+// let key = fs.readFileSync('./src/keygen/id_rsa');
+//
+// let token = jwt.sign(claims, key, {algorithm: "RS256"});
+//
+// let form = {
+//   grant_type: "client_credentials",
+//   scope: "online_access profile",
+//   client_assertion_type: "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
+//   client_id: "8adbf510-2181-4ae3-afd6-277935b6b3cd",
+//   client_secret: "AMJoV_D0jiJeptbBKhxMJCv5tab6CzisnFFU7aeiBZALNiBjPGKu24M07x9fxmCE4SGdlu8vNYN9cPhxDf_K0ko"
+// };
 
 app.use(express.static("node_modules"));
 
